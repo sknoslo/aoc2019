f3eba20cf6c2a3b4ea77772d2ada7c8453a3f829
Steffen Olson <sknoslo@gmail.com>
2019-12-01 09:17:13 -0500

    Project Init and Day 1 solution!

---
+++ app/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import           System.Environment
+
+import           Day01
+
+main :: IO ()
+main = do
+  args <- getArgs
+  contents <- getContents
+  case args of
+    ["1"] -> Day01.solve contents
+    [day] -> putStrLn ("Day " ++ day ++ " not solved yet")
+    _     -> usage
+
+usage :: IO ()
+usage = do
+  putStrLn ""
+  putStrLn "  usage: aoc2019 <day>"
+  putStrLn ""
+  putStrLn ""
+  putStrLn "    example: cat input/01.txt | aoc2019 1"
+  putStrLn ""
---
+++ src/Day01.hs
@@ -0,0 +1,28 @@
+module Day01
+  ( solve
+  ) where
+
+solve :: String -> IO ()
+solve contents = do
+  let masses = parseInput contents
+      partOne = solvePartOne masses
+      partTwo = solvePartTwo masses
+  putStrLn ("Part 1: " ++ show partOne)
+  putStrLn ("Part 2: " ++ show partTwo)
+
+solvePartOne :: [Int] -> Int
+solvePartOne = sum . map fuelRequired
+
+solvePartTwo :: [Int] -> Int
+solvePartTwo = sum . map fuelRequired'
+
+fuelRequired :: Int -> Int
+fuelRequired mass = mass `div` 3 - 2
+
+fuelRequired' :: Int -> Int
+fuelRequired' mass =
+  let initial = fuelRequired mass
+   in sum (takeWhile (> 0) (iterate fuelRequired initial))
+
+parseInput :: String -> [Int]
+parseInput = map read . lines
