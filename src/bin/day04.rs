134e571d2f40225b14b9b42f84952a940342bf6f
Steffen Olson <sknoslo@gmail.com>
2019-12-04 10:45:05 -0500

    finish day 4

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -6,16 +6,18 @@ fn main() -> io::Result<()> {
 
     let (lower, upper) = parse_input(&input);
 
-    println!("{:?} - {:?}", lower, upper);
-
     let p1 = part1(&mut lower.clone(), &mut upper.clone());
 
     println!("part 1: {}", p1);
 
+    let p2 = part2(&mut lower.clone(), &mut upper.clone());
+
+    println!("part 2: {}", p2);
+
     Ok(())
 }
 
-fn part1(lower: &mut Vec<u8>, upper: &mut Vec<u8>) -> usize {
+fn part1(mut lower: &mut Vec<u8>, upper: &mut Vec<u8>) -> usize {
     groom_lower(lower);
     groom_upper(upper);
 
@@ -27,6 +29,25 @@ fn part1(lower: &mut Vec<u8>, upper: &mut Vec<u8>) -> usize {
         }
 
         increment(&mut lower);
+        groom_lower(&mut lower);
+    }
+
+    candidates
+}
+
+fn part2(mut lower: &mut Vec<u8>, upper: &mut Vec<u8>) -> usize {
+    groom_lower(lower);
+    groom_upper(upper);
+
+    let mut candidates = 0;
+
+    while less_than_or_equal(&lower, &upper) {
+        if is_candidate2(&lower) {
+            candidates += 1;
+        }
+
+        increment(&mut lower);
+        groom_lower(&mut lower);
     }
 
     candidates
@@ -65,6 +86,25 @@ fn less_than_or_equal(lower: &Vec<u8>, upper: &Vec<u8>) -> bool {
     true
 }
 
+fn increment(password: &mut Vec<u8>) {
+    for pos in (0..password.len()).rev() {
+        if password[pos] < 9 {
+            password[pos] += 1;
+            break;
+        } else if pos == 0 {
+            // this doesn't handle the case when the first digit needs to be incremented, but this
+            // problem should never run into that case.
+            panic!("whoopsies")
+        }
+
+        password[pos] = 0;
+    }
+}
+
+/**
+ * A possible password is one that contains only increasing (or same) digits from left to right,
+ * with at least one group of matching digits.
+ */
 fn is_candidate(password: &Vec<u8>) -> bool {
     let mut last = 0;
     let mut has_double = false;
@@ -80,6 +120,38 @@ fn is_candidate(password: &Vec<u8>) -> bool {
 
         last = digit;
     }
+
+    has_double
+}
+
+/**
+ * A possible password is one that contains only increasing (or same) digits from left to right,
+ * with at least one group of exactly 2 matching digits.
+ */
+fn is_candidate2(password: &Vec<u8>) -> bool {
+    let mut last = 0;
+    let mut matches = 0;
+    let mut has_double = false;
+
+    for &digit in password {
+        if digit < last {
+            return false;
+        }
+
+        if digit != last {
+            if matches == 1 {
+                has_double = true;
+            }
+
+            matches = 0;
+        } else {
+            matches += 1;
+        }
+
+        last = digit;
+    }
+
+    has_double = has_double || matches == 1; // in case the last two were the double.
 
     has_double
 }
@@ -139,5 +211,35 @@ mod tests {
         let v1 = vec![3, 2, 0];
         let v2 = vec![3, 2, 0];
         assert_eq!(less_than_or_equal(&v1, &v2), true);
+    }
+
+    #[test]
+    fn increment_test() {
+        let mut v1 = vec![6, 9, 9];
+
+        increment(&mut v1);
+
+        assert_eq!(v1, vec![7, 0, 0]);
+    }
+
+    #[test]
+    fn is_candidate2_simple_test() {
+        let v = vec![1, 2, 3, 3, 5];
+
+        assert_eq!(is_candidate2(&v), true);
+    }
+
+    #[test]
+    fn is_candidate2_negative_test() {
+        let v = vec![1, 2, 3, 4, 5];
+
+        assert_eq!(is_candidate2(&v), false);
+    }
+
+    #[test]
+    fn is_candidate2_complex_test() {
+        let v = vec![1, 1, 1, 5, 5];
+
+        assert_eq!(is_candidate2(&v), true);
     }
 }
