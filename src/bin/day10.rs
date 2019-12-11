43aa5880e6ca743bf7e22e9a9e2d277d214fbfa6
Steffen Olson <sknoslo@gmail.com>
2019-12-10 22:57:14 -0500

    had an epiphany about p1 d10, idea for p2 didn't pan out yet

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::collections::HashSet;
 
 fn main() {
@@ -5,83 +6,71 @@ fn main() {
 
     let input = parse_input(input);
 
-    let p1 = part1(&input);
+    let (p1, location) = part1(&input);
 
     println!("part 1: {}", p1);
+
+    let p2 = part2(&input, location);
+
+    println!("part 2: {}", p2);
 }
 
-fn part1(map: &HashSet<(isize, isize)>) -> usize {
+fn part1(map: &HashSet<(isize, isize)>) -> (usize, (isize, isize)) {
     let mut max = 0;
+    let mut max_loc = None;
 
     for a in map.into_iter() {
-        let mut count = 0;
-
-        for b in map.into_iter() {
-            if a == b {
-                continue;
-            }
-
-            let (dx, dy) = get_delta(a, b);
-
-            // nothing can be blocking these
-            if dx == 1 || dy == 1 {
-                count += 1;
-                continue;
-            }
-
-            if let Some(step) = get_step(dx, dy) {
-                // find points on line between a and b and see if they exist in the map.
-                let mut x = a.0 + step.0;
-                let mut y = a.1 + step.1;
-
-                let mut collides = false;
-                let mut loops = 0;
-                while (a.0 < b.0 && x < b.0) || (a.0 > b.0 && x > b.0) {
-                    loops += 1;
-
-                    if loops > 10 {
-                        // println!("artificially breaking looop");
-                        break;
-                    }
-                    // println!("looping");
-                    if map.contains(&(x, y)) {
-                        collides = true;
-                        break;
-                    }
-
-                    x += step.0;
-                    y += step.1;
-                }
-
-                // println!("broke loop");
-
-                if !collides {
-                    count += 1;
-                }
-            } else {
-                // no steps between, so no blocks
-                count += 1;
-            }
-        }
+        let count = map
+            .into_iter()
+            .filter_map(|b| {
+                let (dx, dy) = get_delta(a, b);
+                get_minimal_vector(dx, dy)
+            })
+            .unique()
+            .count();
 
         if count > max {
             max = count;
+            max_loc = Some(a);
         }
     }
 
-    max
+    (max, *max_loc.unwrap())
+}
+
+fn part2(map: &HashSet<(isize, isize)>, a: (isize, isize)) -> isize {
+    let mut points = map
+        .into_iter()
+        .filter_map(|b| {
+            let (dx, dy) = get_delta(&a, b);
+            get_minimal_vector(dx, dy).map(|v| (v, *b))
+        })
+        .collect::<Vec<_>>();
+
+    points.sort_by(|(_, a), (_, b)| a.cmp(b));
+
+    points.sort_by(|((ax, ay), _), ((bx, by), _)| {
+        (*ax as f64)
+            .atan2(*ay as f64)
+            .partial_cmp(&(*bx as f64).atan2(*by as f64))
+            .unwrap()
+    });
+
+    let points: Vec<_> = points.iter().unique_by(|(v, _)| v).collect();
+
+    let twohundredth = points[200].1;
+
+    twohundredth.0 * 100 + twohundredth.1
 }
 
 fn get_delta(a: &(isize, isize), b: &(isize, isize)) -> (isize, isize) {
     (b.0 - a.0, b.1 - a.1)
 }
 
-fn get_step(dx: isize, dy: isize) -> Option<(isize, isize)> {
-    let f = gcd(dx, dy);
+fn get_minimal_vector(dx: isize, dy: isize) -> Option<(isize, isize)> {
+    let f = gcd(dx, dy).abs();
 
-    println!("{}, {}, gcd: {} -> {}, {}", dx, dy, f, dx / f, dy / f);
-
-    if f == 0 || f == 1 {
+    if f == 0 {
         None
     } else {
         Some((dx / f, dy / f))
@@ -122,16 +111,16 @@ mod tests {
     }
 
     #[test]
-    fn get_step_test() {
-        assert_eq!(get_step(6, 9), Some((2, 3)));
-        assert_eq!(get_step(0, 9), Some((0, 1)));
-        assert_eq!(get_step(0, -9), Some((0, -1)));
-        assert_eq!(get_step(9, 0), Some((1, 0)));
-        assert_eq!(get_step(-9, 0), Some((-1, 0)));
-        assert_eq!(get_step(-9, -6), Some((-3, -2)));
+    fn get_min_vector_test() {
+        assert_eq!(get_minimal_vector(6, 9), Some((2, 3)));
+        assert_eq!(get_minimal_vector(0, 9), Some((0, 1)));
+        assert_eq!(get_minimal_vector(0, -9), Some((0, -1)));
+        assert_eq!(get_minimal_vector(9, 0), Some((1, 0)));
+        assert_eq!(get_minimal_vector(-9, 0), Some((-1, 0)));
+        assert_eq!(get_minimal_vector(-9, -6), Some((-3, -2)));
     }
 
-    // #[test]
+    #[test]
     fn part1_test1() {
         let input = parse_input(
             "\
@@ -143,6 +132,88 @@ mod tests {
 ",
         );
 
-        assert_eq!(part1(&input), 8);
+        assert_eq!(part1(&input), (8, (3, 4)));
+    }
+
+    #[test]
+    fn part1_test2() {
+        let input = parse_input(
+            "\
+......#.#.
+#..#.#....
+..#######.
+.#.#.###..
+.#..#.....
+..#....#.#
+#..#....#.
+.##.#..###
+##...#..#.
+.#....####
+",
+        );
+
+        assert_eq!(part1(&input), (33, (5, 8)));
+    }
+
+    #[test]
+    fn part1_test3() {
+        let input = parse_input(
+            "\
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##
+",
+        );
+
+        assert_eq!(part1(&input), (210, (11, 13)));
+    }
+
+    #[test]
+    fn part2_test1() {
+        let input = parse_input(
+            "\
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##
+",
+        );
+
+        let (_, location) = part1(&input);
+
+        assert_eq!(part2(&input, location), 802);
     }
 }
